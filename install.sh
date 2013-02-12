01257657937fd7044c2cd065e006d4c4d0d2d3da
Keir Mierle <mierle@gmail.com>
2013-02-12 09:05:10 +0000

    Overwrite existing configs during install.

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Installs dotfiles into your home directory via links.
-ln -s `pwd`/vimrc ~/.vimrc
-ln -s `pwd`/zshrc ~/.zshrc
-ln -s `pwd`/gitconfig ~/.gitconfig
-ln -s `pwd`/tmux.conf ~/.tmux.conf
+ln -f -s `pwd`/vimrc ~/.vimrc
+ln -f -s `pwd`/zshrc ~/.zshrc
+ln -f -s `pwd`/gitconfig ~/.gitconfig
+ln -f -s `pwd`/tmux.conf ~/.tmux.conf
