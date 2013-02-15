159cab1b6cb6858e419b05c099ff6d7ed48fcf91
Keir Mierle <mierle@gmail.com>
2013-02-15 13:44:59 +0600

    Unify the install script.

--- install.sh
+++ install.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
+set -x
+set -u
 
-# Installs dotfiles into your home directory via links.
+cd
+
+if [ ! -d dotfiles ] ; then
+  git clone git@github.com:keir/dotfiles.git
+fi
+
+cd dotfiles
+
+# Installs dotfiles into your home directory via links, overwriting whatever
+# else is there.
 ln -f -s `pwd`/vimrc ~/.vimrc
 ln -f -s `pwd`/zshrc ~/.zshrc
 ln -f -s `pwd`/gitconfig ~/.gitconfig
--- install_web.sh
+++
@@ -1,7 +0,0 @@
-set -x
-set -u
-
-git clone git@github.com:keir/dotfiles.git
-cd dotfiles
-
-bash install.sh
